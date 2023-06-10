4ec521da92302bb3f62c373104980f026b425de2
Tiago Fonseca <t.fonseca@ua.pt>
2023-06-10 12:59:18 +0100

    Removed a Security Vulnerability

--- app.py
+++ app.py
@@ -38,24 +38,6 @@ def disconnect():
 if __name__ == "__main__":
     socketio.run(app, debug=True, host = "127.0.0.1", port=6070)
 
-# Portas
-# 6070 - Desktop-Tiago
-# 7070 - Laptop-Tiago
-
-# Portas Abertas
-# 6000 - 6100 - Desktop-Tiago
-# 7000 - 7100 - Laptop-Tiago
-
-# Desktop-Tiago
-# 192.168.1.64:6070
-
-# Laptop-Tiago
-# 192.168.1.156:7070
-
-# IP Público
-# 5.249.29.20:6070 - Desktop-Tiago
-# 5.249.29.20:7070 - Laptop-Tiago
-
 
 # Links
 # http://financialtipeco.online:5070/
